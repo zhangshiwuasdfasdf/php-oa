dda96706a349e6d70252f135c2e3a8b87a92341e
2780913982 <2780913982@qq.com>
2016-11-08 09:29:05 +0800

    1

--- App/Lib/Action/ProfileAction.class.php
+++ App/Lib/Action/ProfileAction.class.php
@@ -678,19 +678,18 @@ class ProfileAction extends CommonAction {
 	function status_manage(){
 		//保存员工档案管理状态
 		$user_id=$_POST['user_id'];
+		//dump($_POST);die;
 		$has=M("StatusManage")->where(array("user_id"=>$user_id))->select();
 		if($has){
-			M("StatusManage")->where(array("user_id"=>$user_id))->delete();
-			$data['user_id']=$user_id;
-			$data['no_status']=$_POST['no_status'];
-			$data['stuff_status']=$_POST['stuff_status'];
-			$data['entry_time']=$_POST['entry_time'];
-			$data['regular_time']=$_POST['regular_time'];
-			$data['leave_time']=$_POST['leave_time'];
-			$data['create_name']=get_user_name();
-			$data['create_time']=date('Y-m-d H:i:s',time());
-			$data['remark']=$_POST['remark'];
-			$res=M("StatusManage")->add($data);
+			
+			if(M("StatusManage")->create(I('post.'), 2))
+    		{
+    			if(M("StatusManage")->save() !== FALSE)
+    			{
+    				$this->success('修改成功！', U('lst', array('p' => I('get.p', 1))));
+    				exit;
+    			}
+    		}
 		}else{
 			$data['user_id']=$user_id;
 			$data['no_status']=$_POST['no_status'];
@@ -699,7 +698,7 @@ class ProfileAction extends CommonAction {
 			$data['regular_time']=$_POST['regular_time'];
 			$data['leave_time']=$_POST['leave_time'];
 			$data['create_name']=get_user_name();
-			$data['create_time']=time();
+			$data['create_time']=date('Y-m-d H:i:s',time());
 			$data['remark']=$_POST['remark'];
 			$res=M("StatusManage")->add($data);
 		}
