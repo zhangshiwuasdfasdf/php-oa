e80874f7077e3e17e67f7daf2af98c7c62e9559f
zhangjb <812941112@qq.com>
2016-12-12 17:23:50 +0800

    121202

---
+++ App/Lib/Model/FlowVersionViewModel.class.php
@@ -0,0 +1,20 @@
+<?php
+/*---------------------------------------------------------------------------
+  小微OA系统 - 让工作更轻松快乐 
+
+  Copyright (c) 2013 http://www.smeoa.com All rights reserved.                                             
+
+  Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )  
+
+  Author:  jinzhu.yin<smeoa@qq.com>                         
+
+  Support: https://git.oschina.net/smeoa/smeoa               
+ -------------------------------------------------------------------------*/
+
+class FlowVersionViewModel extends ViewModel {
+	public $viewFields=array(
+		'FlowVersion'=>array('id','flow_type_setting_id','status','version','create_time','version_remark'),
+		'FlowTypeSetting'=>array('flow_name','_on'=>'FlowVersion.flow_type_setting_id=FlowTypeSetting.id')
+		);
+}
+?>
--- App/Lib/Model/UserViewModel.class.php
+++ App/Lib/Model/UserViewModel.class.php
@@ -13,10 +13,15 @@
 
 class UserViewModel extends ViewModel {
 	public $viewFields=array(
-		'User'=>array('id','emp_no','name','letter','dept_id','position_id','pos_id','rank_id','email','duty','office_tel','mobile_tel','pic','bk_pic','birthday','sex','password','is_del','available_hour','more_role','_type'=>'LEFT'),
-		'Position'=>array('name'=>'position_name','sort'=>'position_sort','_on'=>'Position.id=User.position_id','_type'=>'LEFT'),
-		'Rank'=>array('name'=>'rank_name','_on'=>'Rank.id=User.rank_id','_type'=>'LEFT'),
-		'Dept'=>array('name'=>'dept_name','_on'=>'Dept.id=User.dept_id')
+// 		'User'=>array('id','emp_no','name','letter','dept_id','position_id','pos_id','rank_id','email','duty','office_tel','mobile_tel','pic','bk_pic','birthday','sex','password','is_del','available_hour','more_role','_type'=>'LEFT'),
+// 		'Position'=>array('name'=>'position_name','sort'=>'position_sort','_on'=>'Position.id=User.position_id','_type'=>'LEFT'),
+// 		'Rank'=>array('name'=>'rank_name','_on'=>'Rank.id=User.rank_id','_type'=>'LEFT'),
+// 		'Dept'=>array('name'=>'dept_name','_on'=>'Dept.id=User.dept_id'),
+			
+		'User'=>array('id','emp_no','name','letter','email','duty','office_tel','mobile_tel','pic','bk_pic','birthday','sex','password','is_del','available_hour','_type'=>'LEFT'),
+		'RUserPosition'=>array('position_id','dept_id','position_id'=>'pos_id','_on'=>'RUserPosition.user_id=User.id and RUserPosition.is_major=1','_type'=>'LEFT'),
+		'Position'=>array('position_name','_on'=>'RUserPosition.position_id=Position.id','_type'=>'LEFT'),
+		'Dept'=>array('name'=>'dept_name','_on'=>'RUserPosition.dept_id=Dept.id')
 		);
 }
 ?>
