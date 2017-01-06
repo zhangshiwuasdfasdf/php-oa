85eb7442fda077a8f00fde9b9f21b86776ee8525
2780913982 <2780913982@qq.com>
2017-01-06 17:39:00 +0800

    11

--- App/Lib/Action/WorkAgentAction.class.php
+++ App/Lib/Action/WorkAgentAction.class.php
@@ -1,18 +1,5 @@
 <?php
 class WorkAgentAction extends CommonAction {
-	/*function _search_filter(&$map) {
-		$fid=$_REQUEST['fid'];
-		$map['is_del'] = array('eq','0');
-		$map['fid'] = array('eq',$fid);
-		if (!empty($_POST['li_company_id'])) {
-			$map['company_id'] = array('eq',$_POST['li_company_id']);
-		}
-		if (!empty($_POST['li_version'])) {
-			$map['version'] = array('eq',$_POST['li_version']);
-		}
- 		//dump($map);die;
-}*/
-
 	function index(){
 		
 		$this->display();
@@ -21,23 +8,42 @@ class WorkAgentAction extends CommonAction {
 //工作委托
 function agency(){
 	$current_client=$_POST['current_client'];
+	$current_client_emp_no=M("User")->where(array('name'=>$current_client))->getField("emp_no");
 	$to_client=$_POST['to_client'];
+	$to_client_emp_no=M("User")->where(array('name'=>$to_client))->getField("emp_no");
 	$where['step']=array('eq','20');
 	$where['is_del']=array('eq','0');
 	$where['confirm_name']=array('like','%'.$current_client.'%');
-	$data=M("Flow")->field("id,confirm_name")->where($where)->select();
+	$data=M("Flow")->field("id,confirm,confirm_name")->where($where)->select();
+	dump($data);
 	foreach($data as $k=>$v){
-		$name=array_filter(explode('<>',$v['confirm_name']));
+		$name=explode('<>',$v['confirm_name']);
+		$confirm=explode('|',$v['confirm']);
 		foreach($name as $k1=>$v1){
 			if($v1==$current_client){
 				$v1=$to_client;
 			}
+			//替换
 			$name[$k1]=$v1;
 		}
+		foreach($confirm as $k2=>$v2){
+			if($v2==$current_client_emp_no){
+				$v2=$to_client_emp_no;
+			}
+			//替换
+			$confirm[$k2]=$v2;
+		}
 		$confirm_name=implode('<>',$name);
-		$res=M("Flow")->where(array('id'=>$v['id']))->save(array('confirm_name'=>$confirm_name));
+		$confirm=implode('|',$confirm);
+		$id=$v['id'];
+		$where2['flow_id']=array('eq',$v['id']);
+		$where2['user_name']=array('eq',$current_client);
+		$where2['result']=array('EXP','is null');  
+		//dump($confirm);
+		//dump($confirm_name);
+		
 	}
-	dump($res);die;
+	die;
 	if($res){
 		$this->success('委托成功！', U('index'));
     	exit;
